00191ce22c9e94aa81060c13ee20b67db121dec7
Gouthamkumar2011 <gouthamkumarprabhakaran@gmail.com>
2023-07-13 15:13:43 -0400

    js2

--- JavaScript/myscript.js
+++ JavaScript/myscript.js
@@ -1,4 +1,12 @@
-alert("welcome to your bank");
-var deposit = prompt("How much would you like to deposit today");
-alert("You've deposited: " + deposit);
-console.log("You are a cool person")
+// alert("welcome to your bank");
+// var deposit = prompt("How much would you like to deposit today");
+// alert("You've deposited: " + deposit);
+// console.log("You are a cool person")
+
+var num = 1;
+while(num<11){
+    if (num%2 ===0){
+        console.log(num);
+    }
+    num = num+1;
+}
