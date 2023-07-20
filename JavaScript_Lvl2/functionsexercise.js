8de66a1d8f3512b1c1ba91412b30008845227371
Gouthamkumar2011 <gouthamkumarprabhakaran@gmail.com>
2023-07-20 12:21:19 -0400

    functionsexercise

---
+++ JavaScript_Lvl2/functionsexercise.js
@@ -0,0 +1,57 @@
+//Problem 1
+// function Sleepin(weekday, vacation){
+//     return(!weekday || vacation);
+// }
+
+
+//Problem 2
+// function monkeytrouble(aSmile, bSmile){
+//     console.log(aSmile &&bSmile) || (!aSmile && !bSmile);
+// }
+
+// monkeytrouble(true,true)
+
+
+//Problem 3
+// function stringTimes(str, n){
+//     var returnstr = "";
+//     var i = 0;
+//     while (i<n){
+//         returnstr += str;
+//         i++;
+//     }
+//     console.log(returnstr)
+// }
+
+// stringTimes("Hi",4)
+
+//Problem 4
+
+// function luckysum(a,b,c){
+//     if (a === 13){
+//         console.log(0) ;
+//     }else if (b === 13){
+//         console.log(a);
+//     }else if (c === 13){
+//         console.log(a+b);
+//     }else{
+//         console.log(a+b+c);}
+// }
+
+// luckysum(1,2,14)
+
+//Problem 5
+
+function caught_speeding(speed, is_birthday){
+    if (is_birthday === 9){
+        console.log(speed -= 5)
+    }else if(speed <= 60){
+        console.log(0);
+    }else if(60<speed<=80){
+        console.log(1);
+    }else if (speed>=81){
+        console.log(2);
+    }
+}
+
+caught_speeding(155,9)
