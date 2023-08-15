1dbb8cb3f4494ec9e8fc4763b5d139b410007ad9
Gouthamkumar2011 <gouthamkumarprabhakaran@gmail.com>
2023-08-14 23:36:44 -0400

    DOM_1

---
+++ Document_Object_Model/myscript.js
@@ -0,0 +1,24 @@
+var headone = document.querySelector("#one")
+var headtwo = document.querySelector("#two")
+var headthree = document.querySelector("#three")
+
+headone.addEventListener('mouseover',function(){
+    headone.textContent = "Mouse Currently over";
+    headone.style.color = 'red';
+})
+
+
+headone.addEventListener('mouseout',function(){
+    headone.textContent = "Hover over me";
+    headone.style.color = 'black';
+})
+
+headtwo.addEventListener("click",function(){
+    headtwo.textContent = "Clicked ON";
+    headtwo.style.color = 'blue';
+})
+
+headthree.addEventListener("dblclick",function(){
+    headthree.textContent = "I was double clicked";
+    headthree.style.color = 'red';
+})
