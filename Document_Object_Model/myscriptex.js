b8fc11d47a0e014d3880f4b4c2265fb08d416279
Gouthamkumar2011 <gouthamkumarprabhakaran@gmail.com>
2023-08-16 09:38:03 -0400

    modified

--- Document_Object_Model/myscriptex.js
+++ Document_Object_Model/myscriptex.js
@@ -1,26 +1,35 @@
-var restart = document.querySelector("#b")
+// Restart Game Button
+var restart = document.querySelector('#b');
 
-var squares = document.querySelectorAll('td');
+// Grab all the squares
+var squares = document.querySelectorAll("td");
 
-function clearBoard(){
-    for(var i=0;i<squares.length;i++){
-        squares[i].textContent=' ';
-    }
+
+// Clear Squares Function
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].textContent = '';
+  }
+
 }
-
 restart.addEventListener('click',clearBoard)
 
+
+
+
+// Create a function that will check the square marker
 function changeMarker(){
     if(this.textContent === ''){
-        this.textContent = 'X';
-    }else if(this.textContent === 'X'){
-        this.textContent = 'O';
+      this.textContent = 'X';
+      // console.log(marker)
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O';
     }else {
-        this.textContent = '';
+      this.textContent = '';
     }
-}
+};
 
-
-for (var i = 0;i<squares.length;i++){
-    squares[i].addEventListener('click',changeMarker)
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
 }
