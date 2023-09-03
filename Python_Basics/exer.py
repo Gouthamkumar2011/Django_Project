d9a949ca3573f406b817798336d1d25ad1d020bd
Gouthamkumar2011 <gouthamkumarprabhakaran@gmail.com>
2023-09-03 11:29:32 -0400

    exerpython

---
+++ Python_Basics/exer.py
@@ -0,0 +1,22 @@
+# s = 'django'
+
+# print(s[0], s[4],s[:4],s[1:4],s[4:],s[::-1])
+
+# l = [3,7,[1,4,'hello']]
+# print(l)
+# l[2][2] = 'goodbye'
+# print(l)
+
+d1 = {'simple_key':'hello'}
+print(d1['simple_key'])
+
+d2 = {'k1':{'k2':'hello'}}
+print(d2['k1']['k2'])
+
+d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+print(d3['k1'][0]['nest_key'][1][0])
+
+
+age = 4
+name = "sammy"
+print("hello my dog's name is {b} and he is {a} years old".format(a=age, b=name))
